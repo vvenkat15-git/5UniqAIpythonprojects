06e65ac898f86c9728c0ace2564644c366fff4fe
VeNkat <72157165+vvenkat15-git@users.noreply.github.com>
2024-09-10 11:05:19 +0530

    adding project3

---
+++ project3.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import pyttsx3
+import speech_recognition as sr
+import pytz
+import subprocess
+
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+MONTHS = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
